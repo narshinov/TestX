be3523e028431de968eef602606eb8e2dc710b6e
Nikita Arshinov <nikitaarshinov01k@gmail.com>
2025-09-14 23:32:24 +0300

    Initial commit

---
+++ TestX/TestXApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestXApp.swift
+//  TestX
+//
+//  Created by Никита Аршинов on 14.09.25.
+//
+
+import SwiftUI
+
+@main
+struct TestXApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
